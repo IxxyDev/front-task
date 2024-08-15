c81b2387337c9aac22bab3108f852b01f4779d04
1234 <a1234@MacBook-Air-1234.local>
2024-08-15 22:04:22 +0300

    fix: fix semantics a bit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-lg mx-auto py-20">
+  <main class="max-w-lg mx-auto py-20">
     <div class="flex items-center gap-4">
       <div class="bg-gray-300 rounded-full w-14 h-14"></div>
       <NumberInput
@@ -7,7 +7,6 @@
         input-id="hours-old"
         :label="`${student.name} is`"
         description="Student's age in hours"
-
       />
       <p class="text-gray-700">hours old</p>
     </div>
@@ -20,9 +19,9 @@
         description="Student's age in hours"
         disabled
       />
-      <p class="text-gray-700">hours old</p>
+      <span class="text-gray-700">hours old</span>
     </div>
-  </div>
+  </main>
 </template>
 
 <script setup lang="ts">
